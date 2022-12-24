f7b3adf66d516c53d636ecadb26a234b6f5aba5d
semir <seper56@gmail.com>
2022-12-24 11:15:57 -0500

    added inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,17 @@
-// TODO: Include packages needed for this application
+// // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
 // Function call to initialize app
-init();
+
+
+const inquirer = require('inquirer')
+
+console.log('readme gen running')
