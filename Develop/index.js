84e671a3d49a1032ac43dcc4c45d34aae627f11a
semir <seper56@gmail.com>
2023-01-16 19:09:47 -0500

    formatting

--- Develop/index.js
+++ Develop/index.js
@@ -1,17 +1,24 @@
-// // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+// Variables to connect modules to application
+const generateMarkdown = require("./utils/generateMarkdown");
+const licenseBadge = require("./utils/licenseBadge").licenseBadge;
+const questions = require("./utils/questions").questions;
+//Allows for use of async await
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// // TODO: Create an array of questions for user input
-// const questions = [];
+// function to initialize program and create README file
+async function init() {
+  try {
+    const answers = await inquirer.prompt(questions);
+    answers.licenseBadge = licenseBadge(answers.license);
+    let readMeData = generateMarkdown(answers);
+    await writeFileAsync("created-README.md", readMeData);
+  } catch (err) {
+    throw err;
+  }
+}
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// Function call to initialize app
-
-
-const inquirer = require('inquirer')
-
-console.log('readme gen running')
+// function call to initialize program
+init();
